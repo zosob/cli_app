6ab0916a3053ddee3f133d46afd8cc3efffd8e9d
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-01-03 09:04:39 -0600

    Creating a function to make the code better

--- Rust/cli_app/src/main.rs
+++ Rust/cli_app/src/main.rs
@@ -3,14 +3,24 @@ use std::fs; //File system
 
 fn main() {
     let args: Vec<String> = env:: args().collect(); 
-    let query: &String = &args[1];
-    let filename :&String = &args[2];
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file: {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file: {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong!");
 
     println!("With text: \n{}",contents);
+}
+
+struct Config{
+    query : String,
+    filename : String,
+}
+
+fn parse_config(args:&[String]) -> Config{
+    let query: String = args[1].clone();
+    let filename :String = args[2].clone();
+    Config{query, filename}
 }
