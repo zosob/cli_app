ec5f535166c7a293758b6e441126b68ea0e81178
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-01-04 12:25:22 -0600

    Made the main lighter. Added a better error message for no files exist.

--- Rust/cli_app/src/main.rs
+++ Rust/cli_app/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs; //File system
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env:: args().collect(); 
@@ -12,11 +13,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file: {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong!");
+    if let Err(e) = run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config)-> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?; //Automatically returns an error...
 
     println!("With text: \n{}",contents);
+    Ok(())
 }
+
 
 struct Config{
     query : String,
