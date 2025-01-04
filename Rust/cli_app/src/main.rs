a07542f73fdc42e6f5f8720a17cb56988a7a90c5
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-01-04 12:19:57 -0600

    Made a better error message for no arguments.

--- Rust/cli_app/src/main.rs
+++ Rust/cli_app/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs; //File system
+use std::process;
 
 fn main() {
     let args: Vec<String> = env:: args().collect(); 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file: {}", config.filename);
@@ -20,12 +24,12 @@ struct Config{
 }
 
 impl Config{ 
-    fn new(args:&[String]) -> Config{
+    fn new(args:&[String]) -> Result<Config, &str> {
         if args.len() < 3{
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query: String = args[1].clone();
         let filename :String = args[2].clone();
-        Config{query, filename}
+        Ok( Config{query, filename})
     }
 }
