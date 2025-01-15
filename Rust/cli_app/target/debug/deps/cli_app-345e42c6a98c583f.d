3d2cb55e9479f711745757d2794f80f6d875965d
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-01-15 07:35:27 -0600

    Search function created and tests created as well.

---
+++ Rust/cli_app/target/debug/deps/cli_app-345e42c6a98c583f.d
@@ -0,0 +1,5 @@
+/Users/bhaskar-ghosh/Documents/coding/Rust/cli_app/target/debug/deps/cli_app-345e42c6a98c583f: src/main.rs
+
+/Users/bhaskar-ghosh/Documents/coding/Rust/cli_app/target/debug/deps/cli_app-345e42c6a98c583f.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/cli_app/target/debug/deps/cli_app-ec60cb8312c3450b.d
@@ -0,0 +1,5 @@
+/Users/bhaskar-ghosh/Documents/coding/Rust/cli_app/target/debug/deps/cli_app-ec60cb8312c3450b: src/lib.rs
+
+/Users/bhaskar-ghosh/Documents/coding/Rust/cli_app/target/debug/deps/cli_app-ec60cb8312c3450b.d: src/lib.rs
+
+src/lib.rs:
